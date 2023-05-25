18eb93d4bbbcb31098d10a5536f40b4f7bbaf2b0
Jefferson Justo <20b3010072@untels.edu.pe>
2023-05-25 14:51:27 -0500

    Pregunta02 - Listo por Justo

---
+++ Preg02.py
@@ -0,0 +1,10 @@
+#Datos de entrada:
+num1 = 50
+num2 = 20
+num3 = 35
+
+numbers = [num1, num2, num3]
+numbers.sort()
+
+print(" Datos de salida:")
+print("\n",numbers[0],"\n",numbers[1],"\n",numbers[2])
